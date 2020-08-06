bd18adac8e03c1c67ef4e7d0272d148d9f64137d
Francisco Javier Quishpe <fquishpe6288@uta.edu.ec>
2020-08-06 09:17:30 -0500

    CRUD

--- insertarProducto.js
+++ insertarProducto.js
@@ -11,21 +11,18 @@ options :{
 };
 
 
-function ver(){
+function insertar(){
     var conn = new sql.ConnectionPool(dbconfig);
     conn.connect(function (err){
-    if(err){
-    console.log(err);
+    if(err){ console.log(err);
     }else{
         var req= new sql.Request(conn);
         req.query("INSERT INTO dbo.Products  (ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice) "+
         "VALUES('Force',26,5,'Poco',90)"
         ,function(err,recordset){
-            if  (err){
-                console.log(err);
+            if  (err){console.log(err);
             }else{
                 console.log("Se han insertado: "+recordset.rowsAffected+ " filas");
-                conn.close();
                 conn.close();
             }
         });
@@ -34,4 +31,4 @@ function ver(){
 }
 
 
-ver();
+insertar();
